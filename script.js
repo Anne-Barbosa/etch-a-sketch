fef1ef3389e6fe72c3502e7a322e350a835ba76f
Anne <anneconsuelo.95@gmail.com>
2026-08-07 11:26:40 -0300

    Limit grid size to 100x100

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 const container = document.getElementById("container");
+const totalSize = 960;
 
-const rows = 16;
-const cols = 16;
+// CREATE GRID
 
-for (let i = 0; i < rows * cols; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
+function createGrid(size) {
+    container.innerHTML = "";
 
-    square.addEventListener("mouseenter", () => {
-        square.classList.add("rainbow");
-    });
+    const squareSize = totalSize / size;
 
-    container.appendChild(square);
+    for (let i = 0; i < size*size; i++) {
+        const square = document.createElement("div");
+
+        square.classList.add("square");
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.addEventListener ("mouseenter", () => {
+            square.classList.add("rainbow");
+        });
+        container.appendChild(square);
+    }
 }
+
+// FIRST GRID
+
+createGrid(16);
+
+// BUTTON
+const button = document.createElement("button");
+button.textContent = "New Grid";
+
+document.body.appendChild(button);
+
+// RESIZE
+
+button.addEventListener("click", () => {
+    let size = Number(prompt("Enter the number of squares per size (max 100): "));
+
+    if (size < 1 || size > 100 || Number.isNaN(size)) {
+        alert("Please enter a number between 1 and 100");
+        return;
+    }
+    createGrid(size);
+})
 
