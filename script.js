e963508ccb016d7131b2c061f1fd59ba8db89f26
Anne <anneconsuelo.95@gmail.com>
2026-08-07 10:33:14 -0300

    Add hover drawing effect

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ const cols = 16;
 for (let i = 0; i < rows * cols; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+
+    square.addEventListener("mouseenter", () => {
+        square.classList.add("rainbow");
+    });
+
     container.appendChild(square);
 }
+
