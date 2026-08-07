4044a708be784328b1404eaa084edcea43618a56
Anne <anneconsuelo.95@gmail.com>
2026-08-07 10:01:48 -0300

    Generate 16x16 grid with JavaScript

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const container = document.getElementById("container");
 
+const rows = 16;
+const cols = 16;
+
+for (let i = 0; i < rows * cols; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+}
